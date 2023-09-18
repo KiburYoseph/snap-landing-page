c48af2fd061ffecfac59987fb9549536c017d06e
KiburYoseph <kiburstgr@gmail.com>
2023-09-18 12:33:41 +0300

    Nav Done

--- src/Components/LandingContent.jsx
+++ src/Components/LandingContent.jsx
@@ -4,6 +4,8 @@ const LandingContent = () => {
     return (
         <main className='landingContent'>
             <h1>Make<br/> Remote Work</h1>
+            <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
+            <button>Learn More</button>
         </main>
     )
 }
