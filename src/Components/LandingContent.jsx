df1a26916d54048cd248eaaa5474b70bc3a88bf4
KiburYoseph <kiburstgr@gmail.com>
2023-09-18 13:33:44 +0300

    Body Content Responsive

--- src/Components/LandingContent.jsx
+++ src/Components/LandingContent.jsx
@@ -1,5 +1,6 @@
 import '../Components/LandingContent.css';
 import imageDesktop from '../assets/image-hero-desktop.png'
+import imageMobile from '../assets/image-hero-mobile.png'
 import audiophile from '../assets/client-audiophile.svg'
 import databiz from '../assets/client-databiz.svg'
 import maker from '../assets/client-maker.svg'
@@ -10,8 +11,9 @@ const LandingContent = () => {
     return (
         <main className='landingBody'>
             <section className='landingContent'>
-                <h1>Make<br/> remote work</h1>
-                <p>Get your team in sync, no matter your location.<br />Streamline processes, create team rituals, and<br /> watch productivity soar.</p>
+            <img className='mobileImage' src={imageMobile}></img>
+                <h1>Make <span className='break'><br/></span> remote work</h1>
+                <p>Get your team in sync, no matter your location.<span className='break'><br/></span>Streamline processes, create team rituals, and<span className='break'><br/></span> watch productivity soar.</p>
                 <button>Learn more</button>
                 <div className='iconCollection'>
                     <img src={databiz} />
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,14 @@
 import '../Components/Navbar.css';
 import logo from '../assets/logo.svg'
-import arrowUp from '../assets/icon-arrow-up.svg'
-import arrowDown from '../assets/icon-arrow-down.svg'
 
 const Navbar = () => {
     return (
         <nav className='navBar'>
             <img src={logo} className='navLogo'/>
             <ul>
-                <li>Features <img src={arrowDown} /></li>
-                <li>Company <img src={arrowDown} /></li>
-                <li>Careers <img src={arrowDown} /></li>
+                <li>Features <svg width="10" height="6" xmlns="http://www.w3.org/2000/svg"><path stroke="#686868" stroke-width="1.5" fill="none" d="m1 1 4 4 4-4"/></svg></li>
+                <li>Company <svg width="10" height="6" xmlns="http://www.w3.org/2000/svg"><path stroke="#686868" stroke-width="1.5" fill="none" d="m1 1 4 4 4-4"/></svg></li>
+                <li>Careers <svg width="10" height="6" xmlns="http://www.w3.org/2000/svg"><path stroke="#686868" stroke-width="1.5" fill="none" d="m1 1 4 4 4-4"/></svg></li>
                 <li>About</li>
             </ul>
             <div className='sideBtns'>
