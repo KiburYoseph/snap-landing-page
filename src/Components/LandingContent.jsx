c750c4c9967f4dc1c308d1b4366c29acf9cff342
KiburYoseph <kiburstgr@gmail.com>
2023-09-18 12:45:52 +0300

    Body Half Done

--- src/Components/LandingContent.jsx
+++ src/Components/LandingContent.jsx
@@ -1,11 +1,26 @@
 import '../Components/LandingContent.css';
+import imageDesktop from '../assets/image-hero-desktop.png'
+import audiophile from '../assets/client-audiophile.svg'
+import databiz from '../assets/client-databiz.svg'
+import maker from '../assets/client-maker.svg'
+import meet from '../assets/client-meet.svg'
+
 
 const LandingContent = () => {
     return (
-        <main className='landingContent'>
-            <h1>Make<br/> Remote Work</h1>
-            <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
-            <button>Learn More</button>
+        <main className='landingBody'>
+            <section className='landingContent'>
+                <h1>Make<br/> Remote Work</h1>
+                <p>Get your team in sync, no matter your location.<br />Streamline processes, create team rituals, and<br /> watch productivity soar.</p>
+                <button>Learn More</button>
+                <div className='iconCollection'>
+                    <img src={databiz} />
+                    <img src={audiophile} />
+                    <img src={meet} />
+                    <img src={maker} />
+                </div>
+            </section>
+            <img src={imageDesktop}></img>
         </main>
     )
 }
