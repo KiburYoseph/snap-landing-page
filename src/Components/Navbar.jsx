4908b7b19cb5ae1af1850fbfb66a2d8b0e394fee
KiburYoseph <kiburstgr@gmail.com>
2023-09-21 11:11:30 +0300

    Hamburger Menu working

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,128 @@
 import '../Components/Navbar.css';
 import logo from '../assets/logo.svg'
+import todoIcon from '../assets/icon-todo.svg'
+import calendarIcon from '../assets/icon-calendar.svg'
+import reminderIcon from '../assets/icon-reminders.svg'
+import planningIcon from '../assets/icon-planning.svg'
+import menuIcon from '../assets/icon-menu.svg'
+import closeMenuIcon from '../assets/icon-close-menu.svg'
+
+
+
+import { useState, useEffect, useRef } from 'react'
+
+
 
 const Navbar = () => {
+
+    const [features, setFeatures] = useState(false)
+    const [company, setCompany] = useState(false)
+    const [featuresMobile, setFeaturesMobile] = useState(false)
+    const [companyMobile, setCompanyMobile] = useState(false)
+
+    const featuresRef = useRef(null)
+    const companyRef = useRef(null)
+    
+    
+    useEffect(() => {
+        function handleClickOutside(event) {
+          if (featuresRef.current &&
+            !featuresRef.current.contains(event.target) &&
+            companyRef.current &&
+            !companyRef.current.contains(event.target)) {
+            setFeatures(false);
+            setCompany(false);
+          }
+        }
+    
+        window.addEventListener('click', handleClickOutside);
+        return () => {
+        window.removeEventListener('click', handleClickOutside);
+        };
+      }, [featuresRef, companyRef]);
+
+
+
     return (
         <nav className='navBar'>
             <img src={logo} className='navLogo'/>
             <ul>
-                <li>Features <svg width="10" height="6" xmlns="http://www.w3.org/2000/svg"><path stroke="#686868" stroke-width="1.5" fill="none" d="m1 1 4 4 4-4"/></svg></li>
-                <li>Company <svg width="10" height="6" xmlns="http://www.w3.org/2000/svg"><path stroke="#686868" stroke-width="1.5" fill="none" d="m1 1 4 4 4-4"/></svg></li>
-                <li>Careers <svg width="10" height="6" xmlns="http://www.w3.org/2000/svg"><path stroke="#686868" stroke-width="1.5" fill="none" d="m1 1 4 4 4-4"/></svg></li>
+                <li onClick={() => setFeatures(!features)} >Features <svg width="10" height="6" xmlns="http://www.w3.org/2000/svg"><path stroke="#686868" stroke-width="1.5" fill="none" d="m1 1 4 4 4-4"/></svg>      
+                
+                {features ? 
+                
+                <div ref={featuresRef} className='dropDown FeaturesDD'>
+                        <ul className='DDList'>
+                            <li><img className='DDIcons' src={todoIcon} />Todo List</li>
+                            <li><img className='DDIcons' src={calendarIcon} />Calendar</li>
+                            <li><img className='DDIcons' src={reminderIcon} />Reminders</li>
+                            <li><img className='DDIcons' src={planningIcon} />Planning</li>
+                        </ul>
+                    </div>
+                    : null
+                }
+                    
+                              
+                </li>
+                <li onClick={() => setCompany(!company)}>Company <svg width="10" height="6" xmlns="http://www.w3.org/2000/svg"><path stroke="#686868" stroke-width="1.5" fill="none" d="m1 1 4 4 4-4"/></svg>
+                    
+                {company ? 
+                
+                <div ref={companyRef} className='dropDown FeaturesDD'>
+                    <ul className='DDList'>
+                        <li>History</li>
+                        <li>Our Team</li>
+                        <li>Blog</li>
+                    </ul>
+                </div>
+                    : null
+                }
+             
+                </li>
+                <li>Careers</li>
                 <li>About</li>
             </ul>
             <div className='sideBtns'>
                 <button className='loginBtn'>Login</button>
                 <button className='registerBtn'>Register</button>
             </div>
+            <button className='hamburgerButton'><img src={menuIcon}/></button>
+            <ul className='hamburgerContainer'>
+                <li className='closeIcon'><img src={closeMenuIcon}/></li>
+                <li onClick={() => setFeaturesMobile(!featuresMobile)} >Features <svg width="10" height="6" xmlns="http://www.w3.org/2000/svg"><path stroke="#686868" stroke-width="1.5" fill="none" d="m1 1 4 4 4-4"/></svg>                        </li>  
+                    
+                    {featuresMobile ? 
+                    
+                            <ul className='DDList'>
+                                <li><img className='DDIcons' src={todoIcon} />Todo List</li>
+                                <li><img className='DDIcons' src={calendarIcon} />Calendar</li>
+                                <li><img className='DDIcons' src={reminderIcon} />Reminders</li>
+                                <li><img className='DDIcons' src={planningIcon} />Planning</li>
+                            </ul>
+                        : null
+                    }
+                        
+                                
+
+                    <li onClick={() => setCompanyMobile(!companyMobile)}>Company <svg width="10" height="6" xmlns="http://www.w3.org/2000/svg"><path stroke="#686868" stroke-width="1.5" fill="none" d="m1 1 4 4 4-4"/></svg>                    </li>
+                        
+                    {companyMobile ? 
+                    
+                        <ul className='DDList'>
+                            <li>History</li>
+                            <li>Our Team</li>
+                            <li>Blog</li>
+                        </ul>
+                        : null
+                    }
+                
+
+                <li>Careers</li>
+                <li>About</li>
+                <li className='centerButton'><button className='loginBtn'>Login</button></li>
+                <li className='centerButton'><button className='registerBtn'>Register</button></li>
+            </ul>
+
         </nav>
     )
 }
